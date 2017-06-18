12ddda2101e6c07ac516512a3e669aa994be487c
Yuri Karabatov <yuri@yurikarabatov.com>
2017-06-18 23:25:27 +0300

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,10 +10,17 @@ rm -rf "$HOME/.vim"
 rm "$HOME/.vimrc"
 rm "$HOME/.profile"
 rm "$HOME/.tmux.conf"
+rm -rf "$HOME/.config/nvim"
+
+# Check for .config
+if [ -d "$HOME/.config" ]; then
+    mkdir "$HOME/.config"
+fi
 
 # Symlinks
-ln -s "$HOME/homeskel/.vim" "$HOME/.vim"
-ln -s "$HOME/homeskel/.vimrc" "$HOME/.vimrc"
+# ln -s "$HOME/homeskel/.vim" "$HOME/.vim"
+# ln -s "$HOME/homeskel/.vimrc" "$HOME/.vimrc"
 ln -s "$HOME/homeskel/.profile" "$HOME/.profile"
 ln -s "$HOME/homeskel/.tmux.conf" "$HOME/.tmux.conf"
+ln -s "$HOME/homeskel/nvim" "$HOME/.config/nvim"
 source "$HOME/.profile"
