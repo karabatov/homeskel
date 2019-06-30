b987e8bb3fe28166ba7007ebe9489ebe20a280d2
Yuri Karabatov <yuri@yurikarabatov.com>
2019-06-30 14:30:07 +0300

    Mode stuff around to avoid void defuns

--- emacs.d/blog.el
+++ emacs.d/blog.el
@@ -1,40 +1,6 @@
 (require 'seq) ;; For seq-filter.
 (require 'ox-publish) ;; For org-publish-project.
 
-(setq blog-publish-base-directory "~/OneDrive/Writing/Org/")
-(setq blog-publish-export-directory "~/emacs-publish/")
-(setq org-publish-project-alist
-      `(("blog-en"
-	 :components ("blog-articles", "blog-pictures"))
-	("blog-articles"
-	 :base-directory ,blog-publish-base-directory
-	 :exclude ".*"
-	 :include ,(blog-publish-list-files
-		   blog-publish-base-directory
-		   "blog_en_publish")
-	 :publishing-directory ,(concat blog-publish-export-directory "blog/en/")
-
-	 ;; Formatting
-	 :section-numbers nil ;; Don't put numbers on headings
-	 :with-title nil ;; Don't put title in the output
-	 :with-toc nil ;; Don't output TOC
-
-	 ;; HTML
-	 :publishing-function org-html-publish-to-html
-	 :html-postamble ,(lambda (lang) "§ org-publish-project")
-
-	 ;; Sitemap
-	 :auto-sitemap t
-	 :sitemap-filename "index.org"
-	 :sitemap-title "Yuri Karabatov"
-	 :sitemap-sort-files anti-chronologically)
-	("blog-pictures"
-	 :base-directory ,(concat blog-publish-base-directory "Pictures/")
-	 :base-extension ".*"
-	 :publishing-directory ,(concat blog-publish-export-directory "blog/en/Pictures/")
-	 :publishing-function org-publish-attachment
-	 :recursive t)))
-
 (defun blog-publish-blog (project-name)
   "Publishes PROJECT-NAME forcefully."
   (interactive "MProject name: ")
@@ -68,3 +34,37 @@ that include PUBLISH-TAG in the `#+FILETAGS:` property."
 	(if (search-forward-regexp "^\\#\\+filetags:[ ]*\\(.+\\)$" nil t)
             (split-string (match-string 1))
 	  )))))
+
+(setq blog-publish-base-directory "~/OneDrive/Writing/Org/")
+(setq blog-publish-export-directory "~/emacs-publish/")
+(setq org-publish-project-alist
+      `(("blog-en"
+	 :components ("blog-articles", "blog-pictures"))
+	("blog-articles"
+	 :base-directory ,blog-publish-base-directory
+	 :exclude ".*"
+	 :include ,(blog-publish-list-files
+		   blog-publish-base-directory
+		   "blog_en_publish")
+	 :publishing-directory ,(concat blog-publish-export-directory "blog/en/")
+
+	 ;; Formatting
+	 :section-numbers nil ;; Don't put numbers on headings
+	 :with-title nil ;; Don't put title in the output
+	 :with-toc nil ;; Don't output TOC
+
+	 ;; HTML
+	 :publishing-function org-html-publish-to-html
+	 :html-postamble ,(lambda (lang) "<p>§ org-publish-project</p>")
+
+	 ;; Sitemap
+	 :auto-sitemap t
+	 :sitemap-filename "index.org"
+	 :sitemap-title "Yuri Karabatov"
+	 :sitemap-sort-files anti-chronologically)
+	("blog-pictures"
+	 :base-directory ,(concat blog-publish-base-directory "Pictures/")
+	 :base-extension ".*"
+	 :publishing-directory ,(concat blog-publish-export-directory "blog/en/Pictures/")
+	 :publishing-function org-publish-attachment
+	 :recursive t)))
