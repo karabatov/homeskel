6acac32b2080f2bb63454643fe619b3adefe1ff9
Yuri Karabatov <yuri@yurikarabatov.com>
2022-12-27 12:49:51 +0000

    Configure Emacs for Rust

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -262,8 +262,24 @@
 (load (expand-file-name "notes.el" user-emacs-directory))
 
 ;; SLIME
-(load (expand-file-name "slime.el" user-emacs-directory))
+; (load (expand-file-name "slime.el" user-emacs-directory))
+
+;; Rust
+(load (expand-file-name "rust.el" user-emacs-directory))
 
 ;; Blog
 ;; (load "~/.emacs.d/blog.el")
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(toml-mode rust-playground company yasnippet flycheck lsp-ui lsp-mode rustic zetteldeft which-key vertico use-package slime reveal-in-osx-finder protobuf-mode orderless olivetti markdown-mode marginalia magit labburn-theme consult)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
