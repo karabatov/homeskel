af90f0a90cd5ffaa23955fd853ca06a0b9adda4f
Yuri Karabatov <yuri@yurikarabatov.com>
2024-09-27 23:40:11 +0100

    More Neovim

--- nvim/lua/config/neovide.lua
+++ nvim/lua/config/neovide.lua
@@ -1,27 +1,27 @@
 if vim.g.neovide then
-	-- Font
-	vim.o.guifont = "0xProto,Symbols_Nerd_Font_Mono:h16:#e-subpixelantialias"
+    -- Font
+    vim.o.guifont = "0xProto,Symbols_Nerd_Font_Mono:h16:#e-subpixelantialias"
 
-	-- Theme
-	require("zenburn").setup()
+    -- Theme
+    require("zenburn").setup()
 
-	-- Padding
-	vim.g.neovide_padding_top = 10
-	vim.g.neovide_padding_bottom = 10
-	vim.g.neovide_padding_right = 10
-	vim.g.neovide_padding_left = 10
+    -- Padding
+    vim.g.neovide_padding_top = 10
+    vim.g.neovide_padding_bottom = 10
+    vim.g.neovide_padding_right = 10
+    vim.g.neovide_padding_left = 10
 
-	-- Refresh rate when idle
-	vim.g.neovide_refresh_rate_idle = 5
+    -- Refresh rate when idle
+    vim.g.neovide_refresh_rate_idle = 5
 
-	-- Cursor
-	vim.g.neovide_cursor_animation_length = 0.07
-	vim.g.neovide_cursor_trail_size = 0.25
+    -- Cursor
+    vim.g.neovide_cursor_animation_length = 0.07
+    vim.g.neovide_cursor_trail_size = 0.25
 
-	-- Copy/paste
-	vim.keymap.set('v', '<D-c>', '"+y') -- Copy
-	vim.keymap.set('n', '<D-v>', '"+P') -- Paste normal mode
-  	vim.keymap.set('v', '<D-v>', '"+P') -- Paste visual mode
-  	vim.keymap.set('c', '<D-v>', '<C-R>+') -- Paste command mode
-	vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
+    -- Copy/paste
+    vim.keymap.set('v', '<D-c>', '"+y') -- Copy
+    vim.keymap.set('n', '<D-v>', '"+p') -- Paste normal mode
+    vim.keymap.set('v', '<D-v>', '"+p') -- Paste visual mode
+    vim.keymap.set('c', '<D-v>', '<C-r>+') -- Paste command mode
+    vim.keymap.set('i', '<D-v>', '<ESC>l"+Pli') -- Paste insert mode
 end
---
+++ nvim/lua/config/notes.lua
@@ -0,0 +1,6 @@
+-- Notes
+-- https://damien.pobel.fr/post/configure-neovim-vim-gf-javascript-import/
+
+-- Open "2024 master" note
+vim.keymap.set("n", "<F8>", ":cd ~/Documents/notes/ | e 202312302001\\ 2024\\ master.md<CR>")
+
