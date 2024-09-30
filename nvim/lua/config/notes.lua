69c75f9595af315d267e5d0631db82acdacbe654
Yuri Karabatov <yuri@yurikarabatov.com>
2024-09-30 21:27:52 +0100

    Add id copy paste

--- nvim/lua/config/notes.lua
+++ nvim/lua/config/notes.lua
@@ -4,8 +4,21 @@
 -- Open "2024 master" note
 vim.keymap.set("n", "<F8>", ":cd ~/Documents/notes/ | e 202312302001\\ 2024\\ master.md<CR>")
 
--- Find word in file names
+-- Create new note with prompt (New Note)
+vim.keymap.set("n", "<leader>nn", ":cd ~/Documents/notes/<CR>:e <C-r>=strftime(\"%Y%m%d%H%M\")<CR> .md<left><left><left>")
+
+-- SEARCH
+
+-- Find word in file names (Word in Files)
 vim.keymap.set("n", "<leader>wf", ":FzfLua files query=<C-r><C-w><CR>")
 
--- Find word in text
+-- Find word in text (Word in Text)
 vim.keymap.set("n", "<leader>wt", ":FzfLua live_grep search=<C-r><C-w><CR>")
+
+-- COPY ID
+
+-- Copy file id (Id Yank)
+vim.keymap.set("n", "<leader>iy", ":let @i = expand(\"%:t:r:s?\\\\s.*??\")<CR>")
+
+-- Paste file id (Id Paste)
+vim.keymap.set("n", "<leader>ip", "a§<ESC>\"ip")
