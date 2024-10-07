8d943e82aa0d4662d4030fba8296c9566072c16a
Yuri Karabatov <yuri@yurikarabatov.com>
2024-10-07 22:23:02 +0100

    Open random note

--- nvim/lua/config/notes.lua
+++ nvim/lua/config/notes.lua
@@ -1,14 +1,26 @@
 -- Notes
 -- https://damien.pobel.fr/post/configure-neovim-vim-gf-javascript-import/
 
+math.randomseed(os.time())
+local notes = vim.fs.normalize('~/Documents/notes')
+
 -- Open "2024 master" note
-vim.keymap.set("n", "<F8>", ":cd ~/Documents/notes/ | e 202312302001\\ 2024\\ master.md<CR>")
+vim.keymap.set('n', '<F8>', ':cd ' .. notes .. ' | e 202312302001\\ 2024\\ master.md<CR>')
 
 -- Create new note with prompt (New Note)
-vim.keymap.set("n", "<leader>nn", ":cd ~/Documents/notes/<CR>:e <C-r>=strftime(\"%Y%m%d%H%M\")<CR> .md<left><left><left>")
+vim.keymap.set('n', '<leader>nn', ':cd ' .. notes .. '<CR>:e <C-r>=strftime(\"%Y%m%d%H%M\")<CR> .md<left><left><left>')
 
 -- Insert filename (Complete File)
 vim.keymap.set("n", "<leader>cf", ":FzfLua complete-file<CR>")
+
+-- Open random note (Note at Random)
+vim.keymap.set("n", "<leader>nr", function()
+    local all_notes = vim.fs.find(function(name, path)
+        return name:match('.*.md$')
+    end, {limit = math.huge, type = 'file', path = notes})
+    local rand_note = all_notes[math.random(#all_notes)]
+    vim.cmd.edit(rand_note)
+end, { desc = "Open note at random" })
 
 -- SEARCH
 
@@ -18,7 +30,7 @@ vim.keymap.set("n", "<leader>wf", ":FzfLua files query=<C-r><C-w><CR>")
 -- Find word in text (Word in Text)
 vim.keymap.set("n", "<leader>wt", ":FzfLua live_grep search=<C-r><C-w><CR>")
 
--- COPY ID
+-- ID MANIPULATION
 
 -- Copy file id (Id Yank)
 vim.keymap.set("n", "<leader>iy", ":let @i = expand(\"%:t:r:s?\\\\s.*??\")<CR>")
